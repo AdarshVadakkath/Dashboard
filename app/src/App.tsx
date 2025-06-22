5d78fdb12a18f8b4fb99d0b17a84cf0cd1c463f7
Adarshvadakkath <adarshvadakkath2005@gmail.com>
2025-06-22 15:14:37 +0530

    completed

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,31 +1,43 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { Box } from '@mui/material';
-import Sidebar from './components/sidebar';
-import Header from './components/Header';
-import CampaignBanner from './components/CampaignBanner';
-import CampaignsList from './components/CampaignsList';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import CampaignBanner from "./components/CampaignBanner";
+import CampaignsList from "./components/CampaignsList";
 
 const App: React.FC = () => {
   return (
-    <Box sx={{ display: 'flex', minHeight: '100vh', bgcolor: 'background.default' }}>
+    <Box
+      sx={{
+        display: "flex",
+        minHeight: "100vh",
+        bgcolor: "background.default",
+      }}
+    >
       <Sidebar />
-      <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
+      <Box sx={{ flexGrow: 1, display: "flex", flexDirection: "column" }}>
         <Header />
         <Box sx={{ flexGrow: 1, p: 3 }}>
           <Routes>
-            <Route path="/" element={
-              <>
-                <CampaignBanner />
-                <CampaignsList />
-              </>
-            } />
-            <Route path="/campaigns" element={
-              <>
-                <CampaignBanner />
-                <CampaignsList />
-              </>
-            } />
+            <Route
+              path="/"
+              element={
+                <>
+                  <CampaignBanner />
+                  <CampaignsList />
+                </>
+              }
+            />
+            <Route
+              path="/campaigns"
+              element={
+                <>
+                  <CampaignBanner />
+                  <CampaignsList />
+                </>
+              }
+            />
           </Routes>
         </Box>
       </Box>
